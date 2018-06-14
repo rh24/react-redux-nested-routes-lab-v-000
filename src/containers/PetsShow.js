fa8f9a69c2243617cc29af6be42e3ebfcbb3e039
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-14 12:52:59 -0400

    Done.

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const PetsShow = ({ pet }) => 
-  <div className="col-md-8">
-    <h2>{pet.name}</h2>
-    <p>{pet.description}</p>
-  </div>;
+const PetsShow = ({ pet }) => {
+  // debugger;
+  return (
+    <div className="col-md-8">
+      <h2>{pet.name}</h2>
+      <p>{pet.description}</p>
+    </div>
+  )
+}
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    pet: {}
+  const petId = parseInt(ownProps.match.params.petId);
+  const pet = state.pets.find((pet, idx) => pet.id === petId);
+
+  if (pet) {
+    console.log(pet);// {id: 3, name: "Sparky", description: "Orange cat with a laid back attitude"}
+    console.log({ pet }); // pet: {id: 3, name: "Sparky", description: "Orange cat with a laid back attitude"}
+    return { pet };
+  } else {
+    return { pet: {} };
   };
 };
 
